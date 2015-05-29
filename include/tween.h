008701593a8c5c33b35d44d5b611d44284fbe0e0
jxv <jxv@hush.com>
2015-05-29 00:13:08 -0700

    Update to 'ease' nomenclature; Add ease out in

--- include/tween.h
+++ include/tween.h
@@ -1,8 +1,10 @@
 #ifndef TWEEN_H
 #define TWEEN_H
 
-float tw_lerp(float start, float end, float percent);
+float tw_ease(float start, float end, float percent);
 float tw_ease_in(float start, float end, float percent);
 float tw_ease_out(float start, float end, float percent);
+float tw_ease_in_out(float start, float end, float percent);
+float tw_ease_out_in(float start, float end, float percent);
 
 #endif
--- src/tween.c
+++ src/tween.c
@@ -1,7 +1,7 @@
 #include "tween.h"
 #include <math.h>
 
-float tw_lerp(float start, float end, float percent)
+float tw_ease(float start, float end, float percent)
 {
 	return (1.0f - percent) * start + percent * end;
 }
@@ -9,10 +9,30 @@ float tw_lerp(float start, float end, float percent)
 float tw_ease_in(float start, float end, float percent)
 {
 	const float p = 1.0f - percent;
-	return tw_lerp(end, start, p * p);
+	return tw_ease(end, start, p * p);
 }
 
 float tw_ease_out(float start, float end, float percent)
 {
-	return tw_lerp(start, end, percent * percent);
+	return tw_ease(start, end, percent * percent);
+}
+
+float tw_ease_in_out(float start, float end, float percent)
+{
+	const float mid = (start + end) * 0.5f;
+	if (percent < 0.5f) {
+		return tw_ease_in(start, mid, percent * 2.0f);
+	} else {
+		return tw_ease_out(mid, end, (percent - 0.5f) * 2.0f);
+	}
+}
+
+float tw_ease_out_in(float start, float end, float percent)
+{
+	const float mid = (start + end) * 0.5f;
+	if (percent < 0.5f) {
+		return tw_ease_out(start, mid, percent * 2.0f);
+	} else {
+		return tw_ease_in(mid, end, (percent - 0.5f) * 2.0f);
+	}
 }
